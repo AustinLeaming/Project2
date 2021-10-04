323cc403422be047ad813cb5418249f856f59ecb
AustinLeaming <austin@Austins-Macbook-Air.local>
2021-10-04 12:49:17 -0600

    fixed models

--- models/rider.js
+++ models/rider.js
@@ -2,22 +2,39 @@ const mongoose = require('mongoose');
 // all this does is allows us to ref Schema instead of mongoose.Schema
 const Schema = mongoose.Schema
 
+const commentSchema = new Schema({
+    text: String,
+    username: String
+})
+
 const postSchema = new Schema({
     trail: String,
     range: Number,
     text: String,
-    comment: [{}]
+    comments: [commentSchema]
+})
+
+const trailSchema = new Schema({
+    location: String
+})
+
+// riders will 
+const bikeSchema = new Schema({
+    make: String,
+    model: String
 })
 
 // Create your User Model - "Rider"
 const riderSchema = new Schema({
     name: String,
     email: String,
-    trails: [{}],
-    bikes: [{}],
+    trails: [trailSchema],
+    bikes: [bikeSchema],
     posts: [postSchema],
     comment: [],
     googleId: String // <-- required for Oauth 
 })
+
+
 
 module.exports = mongoose.model('Rider', riderSchema)
--- models/trails.js
+++
@@ -1,3 +0,0 @@
-const mongoose = require('mongoose');
-// all this does is allows us to ref Schema instead of mongoose.Schema
-const Schema = mongoose.Schema
