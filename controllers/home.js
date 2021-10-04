544617d491290ebb0f884ee9849168051d05c57f
AustinLeaming <austin@Austins-Macbook-Air.local>
2021-10-04 17:17:44 -0600

    fixed scrolling function

--- controllers/home.js
+++ controllers/home.js
@@ -8,18 +8,13 @@ module.exports = {
 function index(req, res, next) {
   console.log(req.query)
   console.log(req.user)
-  let modelQuery = req.query.name ? {name: new RegExp(req.query.name, 'i')} : {};
-  // Default to sorting by name
-  let sortKey = req.query.sort || 'name';
-  Rider.find(modelQuery)
-  .sort(sortKey).exec(function(err, users) {
+  Rider.find({}, function(err, users) {
     if (err) return next(err);
     // Passing search values, name & sortKey, for use in the EJS
     res.render('home', {
-      users,
       user: req.user,
       name: req.query.name,
-      sortKey
+      riders: users,
     });
   });
 }
