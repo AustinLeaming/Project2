e6e5c57252a58c3ea08d9e2b813ac612d549a589
AustinLeaming <austin@Austins-Macbook-Air.local>
2021-10-06 15:11:24 -0600

    changed color scheme, created buttons for links, created cards for home feed, created card for add a ride

--- controllers/rides.js
+++ controllers/rides.js
@@ -1,6 +1,7 @@
 // not sure if I need to declare both models in here, leaving just in case it comes up
 const User = require('../models/user');
 const Ride = require('../models/rides');
+const { render } = require('../server');
 
 module.exports = {
     index,
@@ -42,14 +43,22 @@ function index(req, res, next) {
 // needs a view 
 // i happen @ localhost:3000/rides/all
 function showAll(req,res){
-    console.log('show')
+    console.log('show function')
+    let rideQuery = req.query.name ? {name: new RegExp(req.query.name, 'i')} : {};
+    Ride.find(rideQuery, function (err, rides) {
+        res.render('rides/all', {
+            rides,
+            user: req.user,
+            nameSearch: req.query.name
+        })
+    })
 }
 
 // render one specific ride, we want to do add a comment to it 
 // needs a view 
 // i happen @ localhost:3000/rides/:id
 function showOne(req,res){
-    console.log('showOne', 'controller, rides, showOne?? why')
+    console.log('showOne')
 }
 
 // render a new page for a user to submit a ride 
@@ -62,7 +71,17 @@ function newRide(req,res){
 // handles the above function form being submitted 
 // no view needed but redirect to localhost:3000/rides - we want to see the new post on my page
 function create(req,res){
-    console.log('create')
+    console.log('create function inside the rides controller')
+    console.log(req.body, '<-- form being submitted')
+    console.log(req.user, '<-- user submitting the form')
+
+    const ride = new Ride(req.body);
+    ride.user = req.user._id;
+    ride.save(function(err) {
+        if (err) return render('rides')
+    })
+    res.redirect('rides/all')
+    
 }
 
 // render an edit page, I want to edit it, or remove it 
--- routes/user.js
+++
@@ -1,6 +0,0 @@
-var router = require('express').Router();
-var riderCtrl = require('../controllers/user');
-
-router.get('/:id', riderCtrl.show)
-
-module.exports = router;
