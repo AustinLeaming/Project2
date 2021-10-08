ed62b325bc7f0d1ac96eefa3fca86def80dd170e
AustinLeaming <austin@Austins-Macbook-Air.local>
2021-10-07 18:11:24 -0600

    ready for presenting on heroku

--- controllers/user.js
+++ controllers/user.js
@@ -1,15 +1,11 @@
-const User = require('../models/user');
+// const User = require('../models/user');
 
-module.exports = {
-    show
-}
+// module.exports = {
+//     show
+// }
 
-function show(req,res){
-  // this function shows all the riders posts 
-  Post.find({user: req.params.id}, callbackFunc)
-  // Rider.findById(req.params.id).exec(function (err,r) {
-  //   // console.log(r, '<--- rider')
-  //   // ^^^^^^ REMOVE AFTER TESTING 
-  //   res.render('show', {r})
-  })
-}
+// function show(req,res){
+//   // this function shows all the riders posts 
+//   Post.find({user: req.params.id}, callbackFunc)
+//   })
+// }
